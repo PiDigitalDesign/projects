9e329c35c7b3873fe750b0ac089780d63f899148
ourlurkio <ourlurkio@gmail.com>
2023-05-31 13:33:20 +0100

    update script to generate template with correct image sizing
    added alt_text to generate alt attribute for img tag

--- scripts/gallery-html-script.py
+++ scripts/gallery-html-script.py
@@ -8,6 +8,7 @@ directoy_to_append = 'assets/img/portfolio/rugs/'
 base_names = []
 #change to whatever filter class to be applied to div template
 filter = "rugs"
+alt_text = "luxurious fine rugs"
 
 
 for filename in os.listdir(directory):
@@ -19,10 +20,14 @@ print(base_names)
 
 for name in base_names:
     div_template = f'''
-                <div class="col-lg-4 col-md-6 portfolio-item filter-{filter}">
-                  <div class="portfolio-wrapper">
-                    <img alt="" class="img-fluid rounded" src="{directoy_to_append}{name}-small.webp"
-                         srcset="{directoy_to_append}{name}-medium.webp 1080w" sizes="80vw">
+                <div class="col-xl-4 col-lg-6 portfolio-item filter-{filter}">
+                  <div class="portfolio-wrapper d-flex justify-content-center">
+                    <img 
+                      alt="{alt_text}" 
+                      class="img-fluid rounded" 
+                      src="{directoy_to_append}{name}-small.webp"
+                      srcset="{directoy_to_append}{name}-small.webp 480w, {directoy_to_append}{name}-medium.webp 1080w" 
+                      sizes="(min-width: 540px) 400px, calc(72.73vw + 22px)">
                   </div>
                 </div>
                 '''
